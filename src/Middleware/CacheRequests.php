e39ddbacc0d3980f8d46724e49fc546f9669bdc7
Barry vd. Heuvel <barryvdh@gmail.com>
2016-08-25 22:39:22 +0200

    Create CacheRequests.php

---
+++ src/Middleware/CacheRequests.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Barryvdh\HttpCache\Middleware;
+
+class CacheRequests
+{
+    public function __construct()
+    {
+        throw new \Exception('This is just a stub class, aliased to the HttpCache in the ServiceProvider');
+    }
+}
