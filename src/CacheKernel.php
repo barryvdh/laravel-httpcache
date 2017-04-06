b784f98faa7448690fef44175d30d4f7de4e0607
Barry vd. Heuvel <barryvdh@gmail.com>
2017-04-06 18:04:02 +0200

    Create CacheKernel.php

---
+++ src/CacheKernel.php
@@ -0,0 +1,45 @@
+<?php namespace Barryvdh\HttpCache;
+
+use Illuminate\Contracts\Http\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpCache\HttpCache;
+use Symfony\Component\HttpKernel\HttpCache\Store;
+use Symfony\Component\HttpKernel\HttpCache\SurrogateInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class CacheKernel implements HttpKernelInterface
+{
+    /** @var  Kernel */
+    protected $kernel;
+
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
+    {
+        return $this->kernel->handle($request);
+    }
+
+    /**
+     * Wrap a Laravel Kernel in a Symfony HttpKernel
+     *
+     * @param Kernel $kernel
+     * @param null $storagePath
+     * @param SurrogateInterface|null $surrogate
+     * @param array $options
+     * @return Kernel|HttpCache
+     */
+    public static function wrap(Kernel $kernel, $storagePath = null, SurrogateInterface $surrogate = null, $options = [] )
+    {
+        $cache = new static($kernel);
+        $store = new Store(storage_path($storagePath ?: storage_path('httpcache')));
+        $kernel = new HttpCache($cache, $store, $surrogate, $options);
+
+        return $kernel;
+    }
+}
