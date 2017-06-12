e60808052dea559a91e10a4bc86138955983d925
Barry vd. Heuvel <barryvdh@gmail.com>
2017-06-12 10:05:35 +0200

    Create SetTtl middleware

---
+++ src/Middleware/SetTtl.php
@@ -0,0 +1,27 @@
+<?php namespace Barryvdh\HttpCache\Middleware;
+
+use Closure;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetTtl
+{
+    /**
+     * Set the Time-To-Live in seconds om the incoming request
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  int $seconds
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $seconds = 60)
+    {
+        $response = $next($request);
+
+        if ($response instanceof Response && $request instanceof  Request && $request->isMethodSafe()) {
+            $response->setTtl($seconds);
+        }
+
+        return $response;
+    }
+}
